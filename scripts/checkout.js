ed8236f77bbd6f4c10034c613d7a988a8bfed5cd
Evan-Ferguson <55107567+Evan-Ferguson@users.noreply.github.com>
2019-10-30 13:46:24 -0600

    update

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,2 +1,29 @@
-console.log('did you change my file name?');
+
 console.log('you made it to the console');
+
+let total = 0.0;
+
+function CM(){
+total = total + 1.99;
+alert("order placed")
+//console.log('total is:' + total)
+}
+
+function C(){
+total = total + 1999.99;
+alert("order placed")
+}
+
+function L(){
+total = total + 100000.00;
+alert("order placed")
+}
+
+function LR(){
+total = total + 200.00;
+alert("order placed")
+}
+
+function Main(){
+document.getElementById('total').innerHTML = Number(total);
+}
